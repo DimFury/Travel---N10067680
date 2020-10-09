9869949991661964389b15152739f1065e2ccd0c
DimFury <56711057+DimFury@users.noreply.github.com>
2020-10-09 16:46:03 +1000

    extra features

--- views.py
+++ views.py
@@ -4,21 +4,15 @@ mainbp = Blueprint('main',__name__)
 
 @mainbp.route('/')
 def index():
-    #if 'email' in session:
-    #    str = '<h1>Hello World' + session['email'] + '</h1>'
-    #else:
-    #    str = '<h1>Hello</h1>'
     return render_template('index.html')
 
-@mainbp.route('/login', methods=['GET','POST'])
-def login():
-    #print(request.values.get('email'))
-    #print(request.values.get('pwd'))
-    session['email']=request.values.get('email')
-    return render_template('login.html')
+# @mainbp.route('/login', methods=['GET','POST'])
+# def login():
+#     session['email']=request.values.get('email')
+#     return render_template('login.html')
 
-@mainbp.route('/logout')
-def logout():
-    if 'email' in session:
-        session.pop('email', None)
-        return 'Session has been cleared'
+# @mainbp.route('/logout')
+# def logout():
+#     if 'email' in session:
+#         session.pop('email', None)
+#         return 'Session has been cleared'
